43a72e6fe46903218af202036de0058ce6226dfe
timotius22 <37534035+timotius22@users.noreply.github.com>
2023-12-14 11:02:33 +0100

    first changes to create a UI

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+// headline
+const headline = document.createElement('h1')
+headline.textContent = "The ultimate Rock Paper Scissors Game";
+document.body.appendChild(headline);
+
+// score board
+const scoreBoard = document.createElement("div");
+const scoreBoardTitle = document.createElement("h2");
+scoreBoardTitle.textContent = "SCOREBOARD";
+scoreBoard.appendChild(scoreBoardTitle);
+const scoreBoardNumbers = document.createElement("div");
+scoreBoardNumbers.style.display = "flex";
+scoreBoard.appendChild(scoreBoardNumbers);
+const scoreBoardPlayer = document.createElement("div")
+scoreBoardPlayer.textContent = "0";
+scoreBoardNumbers.appendChild(scoreBoardPlayer);
+const scoreBoardMiddle = document.createElement("div");
+scoreBoardMiddle.textContent = ":";
+scoreBoardNumbers.appendChild(scoreBoardMiddle);
+const scoreBoardComputer = document.createElement("div")
+scoreBoardComputer.textContent = "0";
+scoreBoardNumbers.appendChild(scoreBoardComputer);
+
+document.body.appendChild(scoreBoard)
+
+// player choice field with button
+const playersChoice = document.createElement("div");
+playersChoice.style.display = "flex"
+
+const rockButton = document.createElement("button");
+rockButton.textContent = "Rock";
+playersChoice.appendChild(rockButton);
+const paperButton = document.createElement("button");
+paperButton.textContent = "Paper";
+playersChoice.appendChild(paperButton);
+const scissorsButton = document.createElement("button");
+scissorsButton.textContent = "Scissors";
+playersChoice.appendChild(scissorsButton);
+
+document.body.appendChild(playersChoice);
+
+// what was picked
+const gameField = document.createElement("div");
+gameField.style.display = "flex";
+const PlayerHasPicked = document.createElement("div");
+PlayerHasPicked.textContent = "???";
+gameField.appendChild(PlayerHasPicked);
+const ComputerHasPicked = document.createElement("div");
+ComputerHasPicked.textContent = "???";
+gameField.appendChild(ComputerHasPicked)
+
+document.body.appendChild(gameField)
+
+
 function getComputerChoice(){
     let randomNumber = Math.ceil(Math.random()*3);
     let computerChoice = "";
@@ -13,19 +67,22 @@ function getComputerChoice(){
     }
 }
 
-function getUserChoice(){
-    userChoice = prompt("Rock, Paper, Scissors");
-    userChoice = userChoice.toLowerCase();
-    if (userChoice === "rock" || userChoice === "paper" || userChoice ==="scissors"){
-        return userChoice
-    } else
-    return "no valid input"
-}
+let userChoice = "";
+const rockButtonClicked = function(){
+    userChoice = "rock";
+    console.log(userChoice);
+};
+
+
+rockButton.addEventListener("click", rockButtonClicked); 
 
 
 
 
-function determineWinner(computerChoice, userChoice){
+
+
+
+/* function determineWinner(computerChoice, userChoice){
     if (computerChoice === userChoice){
         return "No winner"
     } else if ((computerChoice === "rock" && userChoice === "scissors")|| 
@@ -35,9 +92,9 @@ function determineWinner(computerChoice, userChoice){
     } else {
         return "You win"
     }
-}    
+}     */
     
-function playRound(){
+/* function playRound(){
     let userChoice = "";
     let computerChoice = "";
 
@@ -52,8 +109,8 @@ function playRound(){
         console.log(winner);
         alert(winner);
     }
-}
+} */
 
 
-playRound();
+
 
